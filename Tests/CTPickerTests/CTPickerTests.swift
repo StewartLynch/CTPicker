b67efc3f04946dcd672f972b1ad0b18d60c99639
Stewart Lynch <slynch@createchsol.com>
2019-08-15 13:39:14 -0700

    Removed faulty test

--- Tests/CTPickerTests/CTPickerTests.swift
+++ Tests/CTPickerTests/CTPickerTests.swift
@@ -6,7 +6,7 @@ final class CTPickerTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(CTPicker().text, "Hello, World!")
+//        XCTAssertEqual(CTPicker().text, "Hello, World!")
     }
 
     static var allTests = [
